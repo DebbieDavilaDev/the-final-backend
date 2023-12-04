649aecda7712c8ea09b7ba2f34e0bb8581f02804
unknown <debbiedaviladev@gmail.com>
2023-12-03 19:54:54 -0500

    added the category navigation bar

--- functions/index.js
+++ functions/index.js
@@ -6,12 +6,15 @@ import MONGO_URI from "./secrets.js"
 import { service_account } from "./service_account.js"
 import { initializeApp } from "firebase-admin/app"
 
+
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 const client = new MongoClient(MONGO_URI)
 const db = client.db("portfolio")
 const pictures = db.collection("pictures")
+const users = db.collection('users')
 const categories = db.collection("categories")
 const firebaseApp = initializeApp(service_account)
 
@@ -31,6 +34,37 @@ app.post("/upload", async function(req, res){
 
 app.get("/",(req,res)=>{
      res.send("This is working!")
+})
+
+app.post('/signin',async(req,res)=>{ 
+    const {email,password} = req.body
+    // check if user exists
+    const user = await users.findOne({email})
+    if(!user){
+        res.status(404).send({message: 'user not found'})
+        return
+    }
+    // check if password matches
+    if(user.password !== password){
+        res.status(401).send({message: 'password incorrect'})
+        return
+    }
+    // create token
+    const authKey = crypto.randomUUID()
+    await users.updateOne({email},{$set: {authKey}})
+
+    res.status(200).send({token})
+})
+
+app.post('/deleteImage', async (req,res)=>{
+    const url = req.body.url
+    // delete by image url
+    // find image by url
+    const image = await pictures.findOne({url})
+    // delete image
+    await pictures.deleteOne({url})
+    // return all images
+res.status(200).send({message: 'image deleted'})
 })
 
 async function getAllCategories () {
@@ -58,7 +92,7 @@ app.post('/addCategory', async (req, res)=>{
 
 app.post('/deleteCategory', async (req,res)=>{
     const category = req.body.category
-    await categories.deleteOne(category)
+    await categories.deleteOne({category})
 
 const allCategories = await getAllCategories()
 res.status(200).send({message: 'category deleted', categories: allCategories})
